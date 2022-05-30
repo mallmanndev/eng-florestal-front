a37028a3f831794a62ee5db442c4ca8c0ac53eb5
Bruna Vitoria <brunavitoria@github.com>
2022-05-30 19:54:52 -0300

    feat: Cria amostragem dois estágios

--- src/features/enviarAmostragem/templates/forms/index.ts
+++ src/features/enviarAmostragem/templates/forms/index.ts
@@ -1,4 +1,5 @@
 import FormBase from './FormBase';
 import AmostragemSimplesForm from './Simples';
+import AmostragemDoisEstagiosForm from './DoisEstagios';
 
-export { FormBase, AmostragemSimplesForm };
+export { FormBase, AmostragemSimplesForm, AmostragemDoisEstagiosForm };
